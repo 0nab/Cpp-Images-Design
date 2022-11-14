024bc306e810ed365c169836ccbb1a2dd391d959
Soobin Rho <soobinrho@gmail.com>
2022-11-14 16:21:43 -0600

    feat: initial commit
    Co-authored-by: Deepak <77573925+deepakkrish212@users.noreply.github.com>
    Co-authored-by: Marcus <79320268+0nab@users.noreply.github.com>
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ *    Deepak Krishnaa Govindarajan
+ *    Marcus Naess
+ *    Soobin Rho
+ *    November 21, 2022
+ *    COSC 226: C++ Programming
+ *
+ *    Hw7: A Container class for handling images.
+ */
+
+int main() {
+
+
+    // Return 0 to signal success
+    return 0;
+}
