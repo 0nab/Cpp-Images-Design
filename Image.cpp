64fc6e729c71e74e2e56d539681d775e4574adc1
Deepak <dkrishnaagovindarajan21@ole.augie.edu>
2022-11-26 20:48:59 -0600

    changed attribute names

--- Image.cpp
+++ Image.cpp
@@ -8,6 +8,15 @@
  *    Hw: A Container class for handling images.
  */
 
-#include <iostream>  // Required for cout
+#include <iostream> // Required for cout
 #include "Image.h"
 
+using namespace std;
+
+// Implement the Image constructor here
+Image::Image()
+{
+    rows = 0;
+    cols = 0;
+    pixels = vector<int>();
+}
