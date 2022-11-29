aea290122d274d9b8e27e23f43c0d868ee66c648
Soobin Rho <soobinrho@gmail.com>
2022-11-28 23:55:03 -0600

    feat: ostream opeartor overloading for Image
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- main.cpp
+++ main.cpp
@@ -14,58 +14,122 @@
 // declartions to Image.h and finally create main.cpp
 // following the homework instructions.
 
-
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Image {
+private :
+    // Used by the default constructor for creating an empty image
+    const string DEFAULTPGMTYPE {"P2"};
+    static const int DEFAULTCOLUMN {30};
+    static const int DEFAULTROW {10};
+    static const int DEFAULTMAXVALUE {255};
+    static const int DEFAULTCOLORVALUE {0};
 public :
+    /*
+     * PGM IMAGE FILE STRUCTURE
+     * PGM stands for Portale GrayMap format.
+     *
+     * - Starts with the magic number, which indicates the pgm type.
+     *   There are two possible values:
+     *    - `P2` is the human readable version encoded with ASCII.
+     *    - `P4` is encoded in binary, thereby using less disk storage.
+     * - The next two int numbers are totalColumn and totalRow.
+     *   The image dimension is determined by these two numbers.
+     *   These numbers are seperated by a whitespace. The whitespace
+     *   seperating these numbers can be more than one.
+     * - Finally, int maxValue indicates the maximum possible color
+     *   value. In other words, this indicates how many grey colors
+     *   there can be between the black (0) and white (maxValue).
+     * - Every number afterwards is the actual color value for the
+     *   image. In `P2`, every value is seperated by a whitespace.
+     *   In `P4`, however, there's no whitespace. Instead,
+     *   every 8 bit (1 byte) is considerred a color value.
+     * - Plus, any line starting with # is ignored as a comment.
+     *
+     * Example:
+     *   # example.pgm
+     *   P2
+     *   18 7
+     *   255
+     *   0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+     *   0  7  7  7  7  0  0 11 11 11 11  0  0 15 15 15 15  0
+     *   0  7  0  0  0  0  0 11  0  0  0  0  0 15  0  0 15  0
+     *   0  7  7  7  0  0  0 11 11 11  0  0  0 15 15 15 15  0
+     *   0  7  0  0  0  0  0 11  0  0  0  0  0 15  0  0  0  0
+     *   0  7  7  7  7  0  0 11 11 11 11  0  0 15  0  0  0  0
+     *   0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
+     */
+
     using value_type = int;
     using iterator = int*;
 
-private:
-    // ----------------------------------------------------------------
-    // PGM IMAGE FILE STRUCTURE
-    // PGM stands for "Portale Graymap Format"
-    //
-    // - Starts with the magic number, which indicates the pgm type.
-    //   There are two possible values:
-    //    - `P2` is the human readable version encoded with ASCII.
-    //    - `P4` is encoded in binary, thereby using less disk storage.
-    // - The next two int numbers are totalColumn and totalRow.
-    //   The image dimension is determined by these two numbers.
-    //   These numbers are seperated by a whitespace. The whitespace
-    //   seperating these numbers can be more than one.
-    // - Finally, int maxValue indicates the maximum possible color
-    //   value. In other words, this indicates how many grey colors
-    //   there can be between the black (0) and white (maxValue).
-    // - Every number afterwards is the actual color value for the
-    //   image. In `P2`, every value is seperated by a whitespace.
-    //   In `P4`, however, there's no whitespace. Instead,
-    //   every 8 bit (1 byte) is considerred a color value.
-    // - Plus, any line starting with # is ignored as a comment.
-    //
-    // Example:
-    //   P2
-    //   18 7
-    //   255
-    //   0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
-    //   0  7  7  7  7  0  0 11 11 11 11  0  0 15 15 15 15  0
-    //   0  7  0  0  0  0  0 11  0  0  0  0  0 15  0  0 15  0
-    //   0  7  7  7  0  0  0 11 11 11  0  0  0 15 15 15 15  0
-    //   0  7  0  0  0  0  0 11  0  0  0  0  0 15  0  0  0  0
-    //   0  7  7  7  7  0  0 11 11 11 11  0  0 15  0  0  0  0
-    //   0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0
-    // ----------------------------------------------------------------
     std::string pgmType;
     int totalColumn;
     int totalRow;
     int maxValue;
+    int* values;
 
+    // Default constructor creates an empty pgm image
+    Image() :
+        pgmType{DEFAULTPGMTYPE},
+        totalColumn{DEFAULTCOLUMN},
+        totalRow{DEFAULTROW},
+        maxValue{DEFAULTMAXVALUE},
+        values{new int[DEFAULTCOLUMN*DEFAULTROW]} {
+            for (int i=0; i<DEFAULTCOLUMN*DEFAULTROW; ++i) {
+                values[i] = DEFAULTCOLORVALUE;
+            }
+        }
+
+    // Constructor by a constant color value
+    Image(int valueNew) :
+        pgmType{DEFAULTPGMTYPE},
+        totalColumn{DEFAULTCOLUMN},
+        totalRow{DEFAULTROW},
+        maxValue{DEFAULTMAXVALUE},
+        values{new int[DEFAULTCOLUMN*DEFAULTROW]} {
+            for (int i=0; i<DEFAULTCOLUMN*DEFAULTROW; ++i) {
+                values[i] = valueNew;
+            }
+        }
 };
+
+std::ostream& operator<<(std::ostream& ost, const Image image) {
+    /*
+     *
+     */
+    if (image.pgmType=="P2") {
+        // Output the magic number, image dimension, and maximum value
+        cout<<image.pgmType<<'\n'
+            <<image.totalColumn<<' '<<image.totalRow<<'\n'
+            <<image.maxValue;
+
+        // Output all color values
+        int totalPixels = image.totalColumn * image.totalRow;
+        for (int i=0; i<totalPixels; ++i) {
+            if (i%image.totalColumn==0) cout<<'\n';
+            cout<<image.values[i]<<' ';
+        }
+    }
+
+    else if (image.pgmType=="P5") {
+    }
+
+    else {
+
+    }
+
+    return ost;
+}
 
 int main() {
 
+
+    Image image {0};
+
+    cout<<image;
 
 
 
